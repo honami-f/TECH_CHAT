f5168d43665aba2d3b858971bea43a5122d513df
honami-f <hnmr.o.s2@gmail.com>
2023-11-30 21:17:38 +0900

    answerコントローラー設定

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,11 @@
 class AnswersController < ApplicationController
   def create
+    @answer = Answer.create(answer_params)
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:text, :name).merge(post_id: params[:post_id])
   end
 end
